22f44219fa0c1282bd1872613fc25355cd6dfeec
Jonathan Fagan <jonathan@proteinos.com>
2013-08-09 13:46:55 +0100

    First draft of workign site

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,7 +11,7 @@ class PhotosController < ApplicationController
   def create
     @photo = Photo.new(photo_params)
     if @photo.save
-      redirect_to root_path
+      redirect_to photos_path
     else 
       "new"
     end
