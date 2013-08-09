05e5bb86ead5a2a5551a21fe28ad49b25e36a738
Jonathan Fagan <jonathan@proteinos.com>
2013-08-09 14:21:51 +0100

    Blocked the holding page code in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ TestApp::Application.routes.draw do
   
   resources :static_pages
   resources :photos
-  get "/holding", to: "static_pages#holding"
-  root to: "static_pages#holding"
+  # get "/holding", to: "static_pages#holding"
+  root to: "static_pages#index"
   
   
 end
